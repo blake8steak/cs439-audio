a894b7768788e14947c3a5732acab4cf677d2f78
Blake Herrera <blake@blakeherrera.com>
2023-04-14 10:35:12 -0500

    adding info
    comments

--- audio.cc
+++ audio.cc
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fstream>
 using namespace std;
 
+/*
+
+=== things to store ===
+
+- file length (seconds)
+- sample rate (44.1 kHz; 44100 Hz)
+- bit depth (16-bit)
+- is mono/is stereo
+- number of channels
+    * number of samples per channel
+
+*/
