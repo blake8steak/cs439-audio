19c805269604c2bb1589f38cc1a25615e49f7868
Blake Herrera <blake@blakeherrera.com>
2023-04-15 19:45:29 -0500

    outputting simple sound via sdl
    sdl sound output working

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cs439-audio
  Final project for CS439, a C program that can read/play audio files.
+
+ To recompile audio.cc, enter the following command into a terminal:
+### g++ audio.cc -o program -lSDL2
--- audio.cc
+++ audio.cc
@@ -1,18 +1,39 @@
 #include <iostream>
-#include <fstream>
-#include <stdio.h>
-#include <stdlib.h>
-using namespace std;
+#include <cmath>
+#include <vector>
+#include <SDL2/SDL.h>
 
-/*
+const int SAMPLE_RATE = 44100;
+const int AMPLITUDE = 32767;
+const double TWO_PI = 6.283185307179586476925286766559;
 
-=== things to store ===
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_AUDIO);
 
-- file length (seconds)
-- sample rate (44.1 kHz; 44100 Hz)
-- bit depth (16-bit)
-- is mono/is stereo
-- number of channels
-    * number of samples per channel
+    SDL_AudioSpec spec;
+    spec.freq = SAMPLE_RATE;
+    spec.format = AUDIO_S16SYS;
+    spec.channels = 1;
+    spec.samples = 4096;
+    spec.callback = NULL;
 
-*/
+    SDL_AudioDeviceID device = SDL_OpenAudioDevice(NULL, 0, &spec, NULL, 0);
+    SDL_PauseAudioDevice(device, 0);
+
+    std::vector<Uint8> audioBuffer(spec.samples);
+
+    for (int i = 0; i < spec.samples; i++) {
+        double t = (double)i / (double)spec.freq;
+        double frequency = 440.0;
+        double waveform = std::sin(TWO_PI * frequency * t);
+        audioBuffer[i] = AMPLITUDE * waveform;
+    }
+
+    SDL_QueueAudio(device, audioBuffer.data(), audioBuffer.size());
+    SDL_Delay(1000);
+
+    SDL_CloseAudioDevice(device);
+    SDL_Quit();
+
+    return 0;
+}
